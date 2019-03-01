e8e50afb528c70c6e6e999b45d0ed0cc50ccbca6
Xiaoqi Cao <xqcao@thoughtworks.com>
2019-03-01 14:05:04 +0800

    Init project

---
+++ src/main/java/com/xqlibs/spring/domainevent/config/EventConfiguration.java
@@ -0,0 +1,7 @@
+package com.xqlibs.spring.domainevent.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EventConfiguration {
+}
---
+++ src/test/java/com/xqlibs/spring/domainevent/SpringDomainEventApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xqlibs.spring.domainevent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDomainEventApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
